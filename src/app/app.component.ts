cdb16772717b24368eabdcddf259b6afffead84b
Ananya Shahi <shahiananya50@gmail.com>
2025-01-14 19:19:36 +0100

    Added DNA Helix 3D visualisation to Landing Page: Still Need to adjust position such that the DNA helix is on the right and the introduction is on the left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GlowscriptComponent } from './glowscript/glowscript.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlowscriptComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
   title = 'ClinEdit';
 }
